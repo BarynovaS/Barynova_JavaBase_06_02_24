c49e1727816986af28eb49ab6cd94ea7f75129b6
Barynova Sofia <tsonya785@gmail.com>
2024-02-18 21:18:13 +0200

    code final

--- src/main/java/ua/hillel/barynova/lessons/lesson4/Main.java
+++ src/main/java/ua/hillel/barynova/lessons/lesson4/Main.java
@@ -1,4 +1,18 @@
 package ua.hillel.barynova.lessons.lesson4;
 
 public class Main {
-}
+    public static void main(String[] args) {
+        System.out.println();
+        int a = 3790;
+        int b = 64435;
+        int c = 9164;
+        int volume = a*b*c;
+        System.out.println("VOLUME:" +" "+ volume + " "+ "см");
+        System.out.println();
+        int lenght = a+b+c;
+        System.out.println("LENGHT:"+" "+lenght+" "+"см");
+
+
+    }
+    }
+
