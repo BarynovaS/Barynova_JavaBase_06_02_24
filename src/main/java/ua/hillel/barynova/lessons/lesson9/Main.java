d464f883d8a117f1f56f2c99b19894a3d7be5c24
Barynova Sofia <tsonya785@gmail.com>
2024-03-03 12:33:54 +0200

    final code 9

---
+++ src/main/java/ua/hillel/barynova/lessons/lesson9/Main.java
@@ -0,0 +1,65 @@
+package ua.hillel.barynova.lessons.lesson9;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[25];
+        for (int i = 0; i < array.length; i++) {
+            int random = (int) (((Math.random() * 19) + 21));
+            array[i] = random;
+        }
+        System.out.println("Team1 :");
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.print(array[i] + ".");
+            } else {
+                System.out.print(array[i] + ", ");
+
+
+            }
+        }
+
+        int[] array1 = new int[25];
+        for (int j = 0; j < array1.length; j++) {
+            int random = (int) ((Math.random() * 19) + 22);
+            array1[j] = random;
+        }
+        System.out.println();
+        System.out.println("Team2 :");
+
+        for (int j = 0; j < array1.length; j++) {
+            if (j == array1.length - 1) {
+                System.out.print(array1[j] + ".");
+            }else {
+                System.out.print(array1[j] + ", ");
+            }
+        }
+
+        System.out.println();
+        System.out.println();
+
+        int TeamOne = 0;
+        for (int i = 0; i < array.length; i++) {
+            TeamOne += array[i];
+
+        }
+        System.out.println("Sum TeamOne" + TeamOne / 5.0);
+
+        System.out.println();
+
+        int TeamTwo = 0;
+        for (int j = 0; j < array1.length; j++) {
+            TeamTwo += array1[j];
+
+        }
+        System.out.println("Sum TeamTwo" + TeamTwo / 5.0);
+    }
+}
+
+
+
+
+
+
+
+
